64e9846b178e8e8391f1290ff8eea0354952205b
jonghyeok <asdfg9822@naver.com>
2017-03-29 02:41:44 +0900

    함수형 프로그래밍 rename예제를 직접 코딩

--- functional/supplement_study.js
+++ functional/supplement_study.js
@@ -6,7 +6,7 @@
 
 
 /*
-Callback 함수의 인자(Arguments)는 4개를 취함
+Reduce 함수에서 Callback 함수의 인자(Arguments)는 4개를 취함
 
 1) previousValue - 이전 마지막 콜백 호출에서 반환된 값 또는 공금된 경우 initialValue
 2) currentValue - 배열 내 현재 처리되고 있는 요소(element)
@@ -18,23 +18,7 @@ Callback 함수의 인자(Arguments)는 4개를 취함
 var _ = require('underscore');
 var testObj = {name: "ie", userCount:100, maker: "Microsoft", deepObj: { name: 3}};
 
-//Omit - 객체에서 인자로 전달한 인자를 Key로 가진 프로퍼티를 제거함
-var omitObj = (function(){
-    var args = Array.prototype.slice.call(arguments);
-
-    var obj = this;
-    var returnObj = {};
-
-    for(var key in obj) {
-        if(obj.hasOwnProperty(key) && args.indexOf(key) < 0) {
-            returnObj[key] = obj[key];
-        }
-    }
-
-    return returnObj;
-
-}).call(testObj, 'name', 'userCount');
-
+//특정 Object에서 Key를 받아서 제거하는 함수를 만듬
 function omit(targetObj, omitKeys) {
     var returnObj = {};
 
@@ -47,6 +31,7 @@ function omit(targetObj, omitKeys) {
     return returnObj;
 }
 
+//Obj 자신이 가지고 있는 Key들의 배열을 반환
 function getKeys(obj) {
     var resultArr = [];
     for(var key in obj) {
@@ -57,17 +42,27 @@ function getKeys(obj) {
     return resultArr;
 }
 
-function rename(targetObj, renameObj) {
-    return Array.prototype.reduce.call(getKeys(targetObj), function (previousValue, currentValue, currentIndex, originArray) {
-        //
-    }, omit(targetObj, getKeys(targetObj)));
-}
-
-console.log(rename(testObj, {name: "title"}));
-
 /*
 
+Array.prototype.reduce != _.reduce(underscore.js)
+Array.prototype.reduce의 경우에는 배열에서만 사용 가능
+_.reduce는 Object에 대해서도 사용 가능
 
-Array.prototype.reduce.call(null, function(previousValue, currentValue, currentIndex, originArray) {
+*/
 
-}, omitObj);*/
+function rename(targetObj, renameObj) {
+    return _.reduce(targetObj, function (previousValue, currentValue, currentIndex, originArray) {
+
+        if(renameObj.hasOwnProperty(currentIndex)) { //변경할 Key가 target에 존재하면 true
+            previousValue[renameObj[currentIndex]] = currentValue; //변경할 이름으로 현재의 값을 넣어줌
+            return previousValue;
+        } else {
+            return previousValue
+        }
+
+    }, omit(targetObj, getKeys(renameObj))); //defualt값 세팅, 변경할 Key가 있으면 제거함
+}
+
+//omit(testObj, getKeys(testObj));
+
+console.log(rename(testObj, {name: "title"}));
