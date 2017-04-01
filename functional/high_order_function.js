fe27130101cf01091ff90b7b6ca319aed520552b
JongHyeok Choi <asdfg9822@naver.com>
2017-04-01 18:21:53 +0900

    고차 함수를 이용한 최적의 값 구하기 예제

--- functional/high_order_function.js
+++ functional/high_order_function.js
@@ -13,7 +13,7 @@
 var _ = require('underscore');
 
 var result = _.max([1,2,3,4,5]);
-console.log(result);
+//console.log(result);
 
 var people = [{name:'ok', num:10}, {name:'ok2', num:12}];
 //함수에서 전달한 Property를 반환함으로
@@ -21,16 +21,41 @@ var people = [{name:'ok', num:10}, {name:'ok2', num:12}];
 var result = _.max(people, function (obj) {
     return obj.num;
 });
-console.log(result);
+//console.log(result);
 
-function finder(valueFun, bestFun, coll) {
-    return _.reduce(coll, function (best, current) {
-        var bestValue = valueFun(best);
-        var currentValue = valueFun(current);
+//valueFun은 특정 개체에서 원하는 값을 불러오는 역할을 한다
+//bestFun은 최대값을 불러오는 역할을 한다
+//target은 그냥 대상
+function finder(valueFun, bestFun, target) {
+    return _.reduce(target, function(bestValue, currentValue) {
+        var best = valueFun(bestValue);         //최선값을 불러옴
+        var curr = valueFun(currentValue);      //현재값을 불러옴
 
-        return (bestValue === bestFun(bestValue, currentValue)) ? best : current;
-    })
+        return (best === bestFun(best, curr)) ? bestValue : currentValue; //최선값이 기존의 최선값과 같으면 best 아니면 current
+    });
 }
 
-var result = finder(_.identity, Math.max, [1,2,3,4,5]);
+//var result = finder(_.identity, Math.max, [1,2,3,4,5]);
+//console.log(result);
+
+//var result = finder(_.identity, Math.min, [1,2,3,4,5]);
+//console.log(result);
+
+var result = finder(function(obj) {
+    return obj.num;
+}, Math.min, [{num:5, title:'test1'}, {num:6, title:'test2'}]);
+
+function best(fun, coll) {
+    return _.reduce(coll, function(x, y) {
+        return fun(x, y) ? x: y
+    });
+}
+
+/*
+이부분이 왜 위의 함수보다 최적화되는지 생각해볼 필요가 있을듯
+이렇게 되면 _.indentity나, obj.name과 같은 부분과 같이 Value를 구해오는 Function과 best값을 구하는 Function이 혼자되게 된다
+*/
+var result = best(function (x, y) {
+    return x < y;
+}, [1,2,3,4,5])
 console.log(result);
